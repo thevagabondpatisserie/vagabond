53e410b1ba9f8dc051cabf52907edb9ff512012f
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-15 02:10:43 +0700

    Doctype Cai dat bao gia

---
+++ vagabond/vagabond/doctype/bao_gia_cai_dat/bao_gia_cai_dat.py
@@ -0,0 +1,14 @@
+"""Cau chu khung cua to bao gia, khai mot lan dung cho moi to.
+
+Anh Viet 14/08/2026 hoi cac phan nhan cong, van chuyen, set up "phải lưu vào
+đâu để sau này thao tác nhanh". Gia cac khoan do nam ben Thu vien bao gia;
+con CAU CHU khung to - loi mo dau, dieu khoan thanh toan, yeu cau van hanh,
+chinh sach huy, luu y, timeline mau - nam o day. To moi tu chep sang, Loan
+Anh chi sua cho nao khac.
+"""
+
+from frappe.model.document import Document
+
+
+class BaoGiaCaiDat(Document):
+	pass
