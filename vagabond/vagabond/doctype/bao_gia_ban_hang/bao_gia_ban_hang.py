d6376595512fecea0bb83918cbb0f6bedc6a7603
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-14 15:39:58 +0700

    Danh so bao gia BG-YY-MM-NNNNN dem lai tu 1 moi thang

--- vagabond/vagabond/doctype/bao_gia_ban_hang/bao_gia_ban_hang.py
+++ vagabond/vagabond/doctype/bao_gia_ban_hang/bao_gia_ban_hang.py
@@ -6,10 +6,34 @@ nghi thanh toan DNTT-26-08-00001 de ai nhin cung doan duoc thang nao.
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import flt
+from frappe.utils import flt, getdate, nowdate
 
 
 class BaoGiaBanHang(Document):
+	def autoname(self):
+		"""Danh so BG-YY-MM-NNNNN, dem lai tu 1 moi thang.
+
+		Khong dung duoc autoname "format:BG-{YY}-{MM}-{#####}" cua Frappe:
+		trong _format_autoname moi cap ngoac duoc doc RIENG mot lan, nen o
+		{#####} bo dem chay voi tien to rong - tuc la dung chung mot bo dem
+		toan he. To thu dau tien ra thang BG-26-08-00668 chu khong phai
+		00001. Nen sinh ma o day, do chinh cac to BG cung thang ma dem len.
+		"""
+		hn = getdate(self.ngay_bao_gia or nowdate())
+		tien_to = "BG-%02d-%02d-" % (hn.year % 100, hn.month)
+		cuoi = frappe.db.sql(
+			"""select name from `tabBao Gia Ban Hang`
+			where name like %s order by name desc limit 1""",
+			tien_to + "%",
+		)
+		so = 1
+		if cuoi:
+			try:
+				so = int(str(cuoi[0][0]).rsplit("-", 1)[1]) + 1
+			except Exception:
+				so = 1
+		self.name = "%s%05d" % (tien_to, so)
+
 	def validate(self):
 		if self.ten:
 			self.ten = self.ten.strip()
