76a341b1fc856a241cfe37f35c2ca24ecf9e3654
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-16 23:39:41 +0700

    v194: ghep lai app_bep.js

--- vagabond/public/js/app_bep.js
+++ vagabond/public/js/app_bep.js
@@ -11869,7 +11869,7 @@ async function scrVdChiPhi() {
   };
 }
 
-var APPVER = '193';
+var APPVER = '194';
 function freshN() { try { return parseInt(sessionStorage.getItem('vgb_fresh') || '0', 10) || 0; } catch (e) { return 0; } }
 function setFreshN(n) { try { sessionStorage.setItem('vgb_fresh', String(n)); } catch (e) { } }
 function clearFresh() { try { sessionStorage.removeItem('vgb_fresh'); } catch (e) { } }
