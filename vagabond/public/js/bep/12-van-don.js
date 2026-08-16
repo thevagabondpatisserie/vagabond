43d37b4c1286c8397bb0ab2d0ef7c0ea27405a2f
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-16 15:23:35 +0700

    v183: APPVER 183

--- vagabond/public/js/bep/12-van-don.js
+++ vagabond/public/js/bep/12-van-don.js
@@ -716,7 +716,7 @@ async function scrVdChiPhi() {
   };
 }
 
-var APPVER = '182';
+var APPVER = '183';
 function freshN() { try { return parseInt(sessionStorage.getItem('vgb_fresh') || '0', 10) || 0; } catch (e) { return 0; } }
 function setFreshN(n) { try { sessionStorage.setItem('vgb_fresh', String(n)); } catch (e) { } }
 function clearFresh() { try { sessionStorage.removeItem('vgb_fresh'); } catch (e) { } }
