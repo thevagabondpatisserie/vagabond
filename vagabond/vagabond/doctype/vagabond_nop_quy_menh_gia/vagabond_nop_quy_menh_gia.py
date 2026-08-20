de68f807bf99c3db33e06b70e5691190412fc36d
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-20 16:04:43 +0700

    v248 lo 9/10: doctype Vagabond Nop Quy Menh Gia

---
+++ vagabond/vagabond/doctype/vagabond_nop_quy_menh_gia/vagabond_nop_quy_menh_gia.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Lop doctype. Luat nghiep vu nam o vagabond/nop_quy.py."""
+
+from frappe.model.document import Document
+
+
+class VagabondNopQuyMenhGia(Document):
+	pass
