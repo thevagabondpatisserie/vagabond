abc08822f99fbdd68f14a24546b240adcdc7739e
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-08-26 12:16:24 +0700

    v313 lo 3/7: bang Nhat ky tro ly, co cot bao khong huu ich

---
+++ vagabond/vagabond/doctype/vagabond_nhat_ky_tro_ly/vagabond_nhat_ky_tro_ly.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Nhat ky tro ly: moi cau hoi va moi cau tra loi deu nam lai o day.
+
+Khong co ban nay thi khong ai biet tro ly dang day nhan vien dieu gi. Cot
+`bao_loi` do chinh nguoi dung cam co tu nut duoi cau tra loi.
+"""
+
+from frappe.model.document import Document
+
+
+class VagabondNhatKyTroLy(Document):
+	pass
