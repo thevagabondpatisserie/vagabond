bb35812b4fe71662605a782caaedd46385f8c8d5
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-04 19:55:35 +0700

    v418 lo 1: 21 ca kiem cho tab trang thai, o tim va chip dau hieu man Van don

--- vagabond/khung/kiem_thu/chay.py
+++ vagabond/khung/kiem_thu/chay.py
@@ -96,6 +96,7 @@ from vagabond.khung.kiem_thu import (  # noqa: E402,F401
 	thu_unc_va_tk_chi,
 	thu_vai_cua_hang,
 	thu_van_don_dieu_chuyen,
+	thu_van_don_man_ds,
 	thu_viec_can_lam,
 	thu_xung_ho,
 )
