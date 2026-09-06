ee545b5d75167a0ad42afc0a1685c857398bc1b7
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-06 17:31:32 +0700

    v439 lo 7: app_bep.js ghep lai tu bep, khop tung byte

--- vagabond/public/js/app_bep.js
+++ vagabond/public/js/app_bep.js
@@ -21007,7 +21007,7 @@ async function scrVdChiPhi() {
   };
 }
 
-var APPVER = '437';
+var APPVER = '439';
 function freshN() { try { return parseInt(sessionStorage.getItem('vgb_fresh') || '0', 10) || 0; } catch (e) { return 0; } }
 function setFreshN(n) { try { sessionStorage.setItem('vgb_fresh', String(n)); } catch (e) { } }
 function clearFresh() { try { sessionStorage.removeItem('vgb_fresh'); } catch (e) { } }
