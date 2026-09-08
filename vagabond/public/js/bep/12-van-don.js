54f7094f7433fc3c940547b74e40e46950e4f5e8
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-08 20:13:34 +0700

    457 lo 3 bep APPVER 457

--- vagabond/public/js/bep/12-van-don.js
+++ vagabond/public/js/bep/12-van-don.js
@@ -910,7 +910,7 @@ async function scrVdChiPhi() {
   };
 }
 
-var APPVER = '456';
+var APPVER = '457';
 function freshN() { try { return parseInt(sessionStorage.getItem('vgb_fresh') || '0', 10) || 0; } catch (e) { return 0; } }
 function setFreshN(n) { try { sessionStorage.setItem('vgb_fresh', String(n)); } catch (e) { } }
 function clearFresh() { try { sessionStorage.removeItem('vgb_fresh'); } catch (e) { } }
