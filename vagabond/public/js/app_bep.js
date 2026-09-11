aa533d2ca7fe899168b1bea9a7aaceea11b0f0d6
Jin Ho <jin@Jins-MacBook-Pro.local>
2026-09-11 19:25:32 +0700

    chore: danh phien ban 479 cho dot staging

--- vagabond/public/js/app_bep.js
+++ vagabond/public/js/app_bep.js
@@ -21711,7 +21711,7 @@ async function scrVdChiPhi() {
   };
 }
 
-var APPVER = '476';
+var APPVER = '479';
 function freshN() { try { return parseInt(sessionStorage.getItem('vgb_fresh') || '0', 10) || 0; } catch (e) { return 0; } }
 function setFreshN(n) { try { sessionStorage.setItem('vgb_fresh', String(n)); } catch (e) { } }
 function clearFresh() { try { sessionStorage.removeItem('vgb_fresh'); } catch (e) { } }
