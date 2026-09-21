40f2e98a2d22a1908609ba00b8eaf3aab7ff83aa
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-21 17:00:53 +0700

    test(v514): #351 lo 8 - cong kiem them ca hanh vi, nap VIEC_DICH_DANH

--- kiem_truoc_deploy.sh
+++ kiem_truoc_deploy.sh
@@ -122,6 +122,8 @@ node vagabond/khung/kiem_thu/hanh_vi/chay.js
 node vagabond/khung/kiem_thu/hanh_vi/ttnb_317.js
 # v513: man Tra ton kho (chip loc, sap xep, chi tiet ma) chay that tren DOM gia.
 node vagabond/khung/kiem_thu/hanh_vi/tra_ton_513.js
+# #351: man Viec hom nay (Giao viec, Bo qua, Bao xong) chay that tren DOM gia.
+node vagabond/khung/kiem_thu/hanh_vi/bang_sang_351.js
 # Trang /kiem-banh: chuoi go o Huy. Codex doi tren PR #218 mot ca chay that
 # chuoi bam - go - cho phan hoi - kiem so, chu khong do chuoi. Dat o day chu
 # khong de chay tay: mot ca kiem khong nam trong cong la mot ca kiem se quen.
