9d86e9250074e5d25b6aad5868ecfc94ebaa38af
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-23 11:14:15 +0700

    chore(v519): doi loi bao ket qua dong bo cho dung viec moi

--- vagabond/public/js/minvoice_list.js
+++ vagabond/public/js/minvoice_list.js
@@ -61,8 +61,8 @@
 		h += '<tr><td>Tờ mới kéo về</td><td style="text-align:right"><b>' + so(keo.moi) + '</b></td></tr>';
 		h += '<tr><td>Tờ vỏ ruột đã lành</td><td style="text-align:right">' + so(keo.chua_lanh) + '</td></tr>';
 		if (keo.nguon_chua_du) {
-			h += '<tr><td>Nguồn mới có mã, chưa có số/ngày hóa đơn</td><td style="text-align:right">' + so(keo.nguon_chua_du) + '</td></tr>';
-			h += '<tr><td colspan="2">M-Invoice chưa trả đủ nội dung cho các bản ghi này. Hệ giữ mã để kéo lại, chưa thể dựng phiếu mua. Các hóa đơn đủ dữ liệu vẫn xử lý riêng.</td></tr>';
+			h += '<tr><td>Dòng nguồn chỉ có mã, không có số và ngày</td><td style="text-align:right">' + so(keo.nguon_chua_du) + '</td></tr>';
+			h += '<tr><td colspan="2">Các dòng này không phải hóa đơn nên hệ không lưu lại, chỉ đếm. Không cần xử lý gì. Lượt sau nguồn trả đủ nội dung thì tờ đó vào theo đường bình thường.</td></tr>';
 		}
 		if (loi) {
 			h += '<tr><td colspan="2" style="color:#b71c1c">Chưa kéo đủ: ' + frappe.utils.escape_html(loi) + '. Các hóa đơn kéo được vẫn được giữ; cần xử lý lỗi và đồng bộ lại.</td></tr>';
