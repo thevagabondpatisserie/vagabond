0010b761c9b8a512935922a0f7b8dde21ecffffa
thevagabondpatisserie <thevagabondbakery@gmail.com>
2026-09-24 19:18:06 +0700

    v526: patch quyen Repost (lo 2/11)

---
+++ vagabond/patches/hach_toan_thang_v526.py
@@ -0,0 +1,11 @@
+"""v526: quyen Repost cho ke toan FIN (sua tai khoan tren hoa don mua da ghi
+so). Doc vagabond/quyen_ap.py, muc CAP_THEM.
+
+Khong boc try (Codex #364 v2): loi phai lam hong migrate.
+"""
+
+
+def execute():
+	from vagabond import quyen_ap
+
+	return quyen_ap.cap_repost_v526()
